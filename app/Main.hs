ad0ca3034cf913507c16161a4e33303aa44cc805
Chandra Koduru <chandrakoduru@outlook.com>
2018-03-09 18:08:21 +0530

    Have a sample web application running on port 8080

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Args (getArgs)
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
+
+-- import Args (getArgs)
+
+app :: Application
+app _ respond = do
+  putStrLn "Hi!"
+  respond $ responseLBS
+    status200
+    [("Content-Type", "text/plain")]
+    "Hello, Web!"
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn $ show args
+  putStrLn $ "Http Server has started on port 8080"
+  run 8080 app
